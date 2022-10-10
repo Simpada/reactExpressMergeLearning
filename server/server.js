08fb03b87dbbf0e40be2c5e79126383a544011cc
Marcus <60523971+Simpada@users.noreply.github.com>
2022-10-10 19:51:50 +0200

    static page

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const app = express();
 
+/*
 app.get("/", (req, res, next) => {
   res.json({ ooga: " booga" });
 });
+*/
 
 app.get("/login", (req, res, next) => {
   res.json({ username: "Noen Andre" });
@@ -13,6 +15,8 @@ app.get("/login", (req, res, next) => {
 app.post("/login", (req, res, next) => {
   res.sendStatus(401);
 });
+
+app.use(express.static("../client/public"));
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
