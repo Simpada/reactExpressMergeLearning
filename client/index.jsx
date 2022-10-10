fb5c12305fef9f8f38d503ed32ea7f10a5a5eecd
Marcus <60523971+Simpada@users.noreply.github.com>
2022-10-10 20:20:22 +0200

    Integrate react with parcel in express

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById("app"));
+ReactDOM.render(
+  <h1>Hello React with express</h1>,
+  document.getElementById("app")
+);
